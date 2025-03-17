e11992320091e16033a1b3320083dd69f223b221
aaade-pm <Olatunjiademolaimmanuel@gmail.com>
2025-03-17 12:43:49 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <main className="h-screen w-full flex justify-center items-center">
+        <h1 className="text-7xl font-extrabold text-center font-ojuju">
+          Welcome to the new vibrant homes project.
+        </h1>
+      </main>
+    </>
+  );
+};
+
+export default App;
